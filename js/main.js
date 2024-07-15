fdaaab1be6dbf4e679fbd9b8359d61d2b78c7ac2
Denise Pingiori <pingiorid@gmail.com>
2024-07-15 20:32:52 +0200

    [upd] error

--- js/main.js
+++ js/main.js
@@ -56,11 +56,42 @@ const posts = [
     }
 ];
 
-
-//recupero elemento del dom che deve contenere i post 
+//recupero l'elem del dom che deve contenere i posts
 const container = document.getElementById('container');
 
-//ciclo i post per metterli dentro il container di sopra
+//ciclo i posts per metterli dentro il container di sopra
 posts.forEach((post) => {
-    container.innerHTML += post.content
-})
+
+
+    container.innerHTML += `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.media}" alt="{$post.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">{$post.author.name}</div>
+                        <div class="post-meta__time">4 mesi fa</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?image=171" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`;
+});
